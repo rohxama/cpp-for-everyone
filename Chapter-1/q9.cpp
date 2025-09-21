61bef91258e11fa741779b9beb6fd167a8125f93
rohxama <rohxama@gmail.com>
2025-09-21 11:44:52 +0500

    q9 done

---
+++ Chapter-1/q9.cpp
@@ -0,0 +1,13 @@
+// Program: Write a program that prints a face
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << " \\\\\\  " << endl;
+    cout << " +-----+" << endl;
+    cout << "(| 0 0 |)" << endl;
+    cout << " |  ^  |" << endl;
+    cout << " | '-' |" << endl;
+    cout << " +-----+" << endl;
+    return 0;
+}
