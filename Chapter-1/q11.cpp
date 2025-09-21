9b402b73cd25e3cf2dbcc0b1c3f2ce187f28af6a
rohxama <rohxama@gmail.com>
2025-09-21 11:56:07 +0500

    q11 done

---
+++ Chapter-1/q11.cpp
@@ -0,0 +1,13 @@
+// Program: Write a program that prints an animal.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "   /\_/\      -----" << endl;
+    cout << "  ( ' ' )    / Hi  \\" << endl;
+    cout << "  (  -  )    < Roma |" << endl;
+    cout << "   | | |    \\ Coder! /" << endl;
+    cout << "  (__|__)     -----" << endl;
+    return 0;
+}
