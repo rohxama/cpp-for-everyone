55dafa866a0e8580edb49808dfddd7dd9f78233b
rohxama <rohxama@gmail.com>
2025-09-21 12:05:11 +0500

    q14 done

---
+++ Chapter-1/q14.cpp
@@ -0,0 +1,14 @@
+// Program: Prints a Piet Mondrian painting design
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "########|@@@@@@@|:::::::" << endl;
+    cout << "########|@@@@@@@|:::::::" << endl;
+    cout << "########|-------|:::::::" << endl;
+    cout << "|||||||||@@@@@@@|:::::::" << endl;
+    cout << "|||||||||@@@@@@@|:::::::" << endl;
+    cout << "|||||||||-------|:::::::" << endl;
+    return 0;
+}
