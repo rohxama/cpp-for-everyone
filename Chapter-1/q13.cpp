95e05d0e05426e3ca044fb35dbda1f32957caa41
rohxama <rohxama@gmail.com>
2025-09-21 12:03:37 +0500

    q13 done

---
+++ Chapter-1/q13.cpp
@@ -0,0 +1,12 @@
+// Program: Prints the favrt poem
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Twinkle, twinkle, little star," << endl;
+    cout << "How I wonder what you are!" << endl;
+    cout << "Up above the world so high," << endl;
+    cout << "Like a diamond in the sky." << endl;
+    return 0;
+}
