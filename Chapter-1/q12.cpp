677778816798fba8d86bb97dd299798408071234
rohxama <rohxama@gmail.com>
2025-09-21 12:01:26 +0500

    q12 done

---
+++ Chapter-1/q12.cpp
@@ -0,0 +1,11 @@
+// Program: Prints the names of three favrt movies
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "KGF" << endl;
+    cout << "Master" << endl;
+    cout << "Beast" << endl;
+    return 0;
+}
