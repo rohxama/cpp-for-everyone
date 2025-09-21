c38de05cd103d169e5137d5d44629a3bb2115df0
rohxama <rohxama@gmail.com>
2025-09-21 11:31:36 +0500

    q8 done

---
+++ Chapter-1/q8.cpp
@@ -0,0 +1,13 @@
+// Program: Prints the name in large letters using stars
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "*****   *****   *   *   *****" << endl;
+    cout << "*   *   *   *   ** **   *   *" << endl;
+    cout << "*****   *   *   * * *   *****" << endl;
+    cout << "* *     *   *   *   *   *   *" << endl;
+    cout << "*  **   *****   *   *   *   *" << endl;
+    return 0;
+}
